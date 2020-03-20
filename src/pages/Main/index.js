90469408a02a7eedccaa2f83991380074d6904bf
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-03-20 17:27:46 -0300

    feat: add global styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,9 +4,6 @@ import { Title } from './styles';
 
 export default function Main() {
   return (
-    <Title error={false}>
-      Main
-      <small>menor</small>
-    </Title>
+    <Title>Hello World</Title>
   );
 }
