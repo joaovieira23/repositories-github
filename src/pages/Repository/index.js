12959047816fee2d69a4f80545171fb3980629bf
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-03-21 18:00:59 -0300

    feat: add proptypes definition

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 // import { Container } from './styles';
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
   state = {
     repository: {},
     issue: [],
     loading: true,
-  }
+  };
 
   async componentDidMount() {
     const { match } = this.props;
